11603a7e29c3299bc5a2e34a246ed1d0531d7ba4
jez <jez@jezfx.com>
2025-06-15 22:10:43 +0100

    build

---
+++ apps/web/.open-next/assets/_next/static/chunks/pages/_app-4db5468a0f3708b8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1041:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7390)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1041),n(1400))),_N_E=_.O()}]);
